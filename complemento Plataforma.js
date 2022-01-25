987f7f47f368c106f2ae33d0ed473ec873aacd13
CarolineAPG <karol.alice1999@gmail.com>
2022-01-25 11:11:57 -0300

    main

--- complemento Plataforma.js
+++
@@ -1,9 +0,0 @@
-const body =document.querySelector("body"),
-      sidebar =body.querySelector(".sidebar"),
-      toggle =body.querySelector(".toggle"),
-      modeSwitch =body.querySelector(".toggle-switch"),
-      modeText =body.querySelector(".mode text");
-
-      modeSwitch.addEventListener("click", () =>{
-        body.classList.toggle("dark");
-      });
