2c4fa25827370db0049651b630c266782991f8ed
CarolineAPG <karol.alice1999@gmail.com>
2022-02-15 11:09:39 -0300

    main

--- Tabela Ingredientes/main.js
+++ Tabela Ingredientes/main.js
@@ -6,6 +6,16 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "ingrediente1",
+    quantidade: "150g",
+    preço: "13,00"
+}
+ //evento
+ const createClient = (client) => {
+   localStorage.setItem("name", "quantidade")
+ }
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
