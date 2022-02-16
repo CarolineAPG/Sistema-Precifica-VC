fcf1560621692a4df456c2edd3821a1a94828ab6
CarolineAPG <karol.alice1999@gmail.com>
2022-02-16 11:09:48 -0300

    main

--- Tabela Ingredientes/main.js
+++ Tabela Ingredientes/main.js
@@ -1,25 +1,93 @@
 'use strict'
 
 const openModal = () => document.getElementById('modal')
-    .clasList.add('active')
+    .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
-
-const tempClient = {
-    nome: "iarroz",
-    quantidade: "160g",
-    preço: "13,00"
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal').classList.remove('active')
 }
-//evento
+
+  //se const db_client nao tiver nada (??) então mande nada []
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+
+//delet:
+const deleteClient = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index,1)
+    setLocalStorage(dbClient)
+}
+
+//update:
+const updateClient = (index, client) => {
+      const dbClient = readClient()
+      dbClient[index] = client
+      setLocalStorage(dbClient)
+}
+
+
+//Read : essa função lê o dado cadastrado
+const readClient = () => getLocalStorage()
+
+
+
+//CRUD Create
 const createClient = (client) => {
-    const db_client = JSON.parse(localStorage.getItem('db_client'))
-    db_client.push(client)
-    localStorage.setItem("db_client", JSON.stringify(db_client))
+    const dbClient = getLocalStorage()
+    dbClient.push (client)
+    setLocalStorage(dbClient)
+    
 }
+
+const isValidFields = ()=> {
+    return document.getElementById("form").reportValidity()
+}
+
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.Value = "")
+}
+
+//interação com layout
+const saveClient = () => {
+    if (isValidFields()){
+        const client= {
+        nome: document.getElementById('nome').Value,
+        quantidade: document.getElementById('quantidade').Value,
+        preço: document.getElementById('preço').Value
+    }
+    createClient(client)
+    closeModal()
+    }
+}
+
+const createRow = (client) => {
+      const newRow = document.createElement('tr')
+      newRow.innerHTML = `
+      <td>${client.nome}</td>
+      <td>${client.quantidade}</td>
+      <td>${client.preço}</td>
+      <td>
+          <button type="button" class="button green">editar</button>
+          <button type="button" class="button red">excluir</button>
+      </td>
+      `
+      document.querySelector("#tableClient>tbody").appendChild(newRow)
+}
+
+const updateTable = () =>{
+    const dbClient = readClient()
+    dbClient.forEach(createRow)
+}
+
+updateTable()
 
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+document.getElementById('Salvar')
+         .addEventListener('click', saveClient)
